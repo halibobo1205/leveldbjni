e3fef831062d58245792f824d7286814b96b5453
halibobo1205 <halibobo1205@gmail.com>
2021-06-30 15:28:39 +0800

    fix  libstdc++.so.6.0.22 ELF file's phentsize not the expected size

--- leveldbjni/src/main/java/org/fusesource/leveldbjni/load/LoadLibrary.java
+++ leveldbjni/src/main/java/org/fusesource/leveldbjni/load/LoadLibrary.java
@@ -10,10 +10,10 @@ import org.fusesource.leveldbjni.load.util.OSinfo;
 public class LoadLibrary {
 
   private static final String LIB_STD_X86_64 = "/usr/lib64/libstdc++.so.6.0.22";
-  private static final String JAR_PATH_X86_64 = "/lib/libstdc++.so.6.0.22";
+  private static final String JAR_PATH_X86_64 = "/lib/linux64/libstdc++_6.0.22.so";
   //TODO
   private static final String LIB_STD_X86 = "/usr/lib/libstdc++.so.6.0.22";
-  private static final String JAR_PATH_X86 = "/lib/linux32/libstdc++.so.6.0.22";
+  private static final String JAR_PATH_X86 = "/lib/linux32/libstdc++_6.0.22.so";
 
   public static void load() {
     try {
