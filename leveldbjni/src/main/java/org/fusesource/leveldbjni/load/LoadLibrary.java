373b517c15f42f56ae7e3f1684571da5af7d479e
halibobo1205 <halibobo1205@gmail.com>
2021-07-02 16:06:27 +0800

    avoid Version `GLIBCXX_3.4.22' not found may be appear for some unknown reason

--- leveldbjni/src/main/java/org/fusesource/leveldbjni/load/LoadLibrary.java
+++ leveldbjni/src/main/java/org/fusesource/leveldbjni/load/LoadLibrary.java
@@ -12,6 +12,9 @@ public class LoadLibrary {
 
   private static final String LIB_STD_X86_64 = "/usr/lib64/libstdc++.so.6.0.22";
   private static final String JAR_PATH_X86_64 = "/lib/linux64/libstdc++_6.0.22.so";
+  private static final String LEVELDB_JAR_PATH = "/META-INF/native/linux64/libleveldbjni.so";
+  private static final String LIB_LEVELDB_JNI_SO = "/usr/lib64/libleveldbjni.so";
+
   //TODO
   private static final String LIB_STD_X86 = "/usr/lib/libstdc++.so.6.0.22";
   private static final String JAR_PATH_X86 = "/lib/linux32/libstdc++_6.0.22.so";
@@ -27,8 +30,12 @@ public class LoadLibrary {
       try {
         if (OSinfo.isLinux() && OSinfo.isX64()) {
           File file = new File(LIB_STD_X86_64);
+          File libLevelDBJNI = new File(LIB_LEVELDB_JNI_SO);
           if (!file.exists()) {
             NativeUtils.loadLibraryFromJar(JAR_PATH_X86_64);
+          }
+          if (!libLevelDBJNI.exists()) {
+            NativeUtils.loadLibraryFromJar(LEVELDB_JAR_PATH);
           }
         }
         isLoaded = true;
